be0b4952303365492958e7861ff8aee5d9440a34
OGLernings <146085572+OGLernings@users.noreply.github.com>
2023-10-31 11:53:43 -0400

    Update lambda_function.py

--- lib/services/lambda_function.py
+++ lib/services/lambda_function.py
@@ -1,7 +1,7 @@
 import json
 import os
 def lambda_handler(event, context):
-    message = 'Hello, Good Morning!!' 
+    message = 'Hello, Good Evening!!' 
     return { 
         'message' : message
     }
